924410fa8101c82ab5eee482adfd5c82a0802def
Meredith Bowden <meredith.bowden@TRG-Meredith-Bowden.local>
2024-05-09 14:11:27 -0500

    did stuff

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use-strict";
+
+(() => {
+
+document.querySelector("#testButton").addEventListener("click", handleClick);
+
+function handleClick(){
+    console.log("You've been quacked");
+}
+})();
