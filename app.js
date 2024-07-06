7975b5e32106075cc7757671feb3b150438c691d
Meredith Bowden <meredith.bowden@TRG-Meredith-Bowden.local>
2024-07-06 10:35:43 -0500

    Installed mySQL

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
 
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index.ejs");
 });
 
 app.listen(port, () => {
