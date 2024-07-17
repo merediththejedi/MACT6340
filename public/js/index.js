c2b4c3d044582f0904248c378dcd80340ee7073c
Meredith Bowden <meredith.bowden@TRG-Meredith-Bowden.local>
2024-07-16 20:13:47 -0500

    connect wallet, contact page, about styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,54 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM fully loaded and parsed");
+
+    let userAddress = null;
+    let connect = document.querySelector("#wallet-connect");
+
+    if (!connect) {
+        console.error("Connect Wallet button not found");
+        return;
+    }
+
+    console.log("Connect Wallet button found");
+
+    if (typeof window.ethereum === 'undefined') {
+        console.error('MetaMask is not installed!');
+        connect.innerHTML = "MetaMask Not Installed";
+        return;
+    }
+
+    console.log("MetaMask is installed");
+
+    connect.addEventListener("click", async (event) => {
+        event.preventDefault();  // Prevent the default anchor behavior
+        console.log("Connect Wallet button clicked");
+        await connectWallet();
+    });
+
+    async function connectWallet() {
+        try {
+            console.log("Requesting wallet connection");
+            const data = await window.ethereum.request({ method: "eth_requestAccounts" });
+            userAddress = data[0];
+            let walletString = userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+            connect.innerHTML = walletString;
+            console.log("Wallet connected:", userAddress);
+            return userAddress;
+        } catch (err) {
+            if (err.code === 4001) {
+                // EIP-1193 userRejectedRequest error
+                console.log("Please connect a wallet.");
+            } else {
+                console.error(err);
+            }
+        }
+    }
+});
+
+
+
 import express from 'express';
 import dotenv from 'dotenv';
 import * as utils from './utils/utils.js';
@@ -10,6 +61,8 @@ const port = 3000; // Changed port number
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
+
+
 
 // Connect to the database when the server starts
 connect().then(() => {
@@ -37,3 +90,4 @@ app.get("/projects", (req, res) => {
 app.listen(port, () => {
     console.log(`App running @ http://localhost:${port}`);
 });
+
